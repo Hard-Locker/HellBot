b34bf0c01563f087011f3088d2c30b08fda93b75
HALO-PC <nekitzollin@gmail.com>
2024-04-20 21:22:58 +0300

    0.2.42 - Change Lavaplayer

--- Hell Player/src/main/java/halot/nikitazolin/bot/discord/audio/player/QueueFiller.java
+++ Hell Player/src/main/java/halot/nikitazolin/bot/discord/audio/player/QueueFiller.java
@@ -1,43 +1,69 @@
 package halot.nikitazolin.bot.discord.audio.player;
 
-import org.springframework.context.annotation.Scope;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
-@Scope("prototype")
 @Slf4j
 @RequiredArgsConstructor
-public class QueueFiller implements AudioLoadResultHandler {
+public class QueueFiller {
 
-  private final IPlayerService playerManager;
+  private final IPlayerService playerService;
 
-  @Override
-  public void trackLoaded(AudioTrack track) {
-    playerManager.getQueue().add(track);
+  public void makeTracks(List<String> identifiers) {
+    List<AudioReference> references = new ArrayList<>();
 
-    log.debug("Add track: " + track);
+    for (String url : identifiers) {
+      references.add(new AudioReference(url, null));
+    }
+
+    log.debug("");
+
+    for (AudioReference reference : references) {
+      
+//      playerService.getAudioPlayerManager().loadItemOrdered("Key", reference, new TrackLoader(playerService));
+    }
   }
 
-  @Override
-  public void playlistLoaded(AudioPlaylist playlist) {
+  
+  
+  
+  @RequiredArgsConstructor
+  private class TrackLoader implements AudioLoadResultHandler {
 
-  }
+    private final IPlayerService playerService;
 
-  @Override
-  public void noMatches() {
+    @Override
+    public void trackLoaded(AudioTrack track) {
+      playerService.getQueue().add(track);
 
-  }
+      log.debug("Add track: " + track);
+    }
 
-  @Override
-  public void loadFailed(FriendlyException exception) {
+    @Override
+    public void playlistLoaded(AudioPlaylist playlist) {
 
+    }
+
+    @Override
+    public void noMatches() {
+
+    }
+
+    @Override
+    public void loadFailed(FriendlyException exception) {
+
+    }
   }
 }
--- Hell Player/src/main/java/halot/nikitazolin/bot/discord/command/commands/music/PlayCommand.java
+++ Hell Player/src/main/java/halot/nikitazolin/bot/discord/command/commands/music/PlayCommand.java
@@ -1,19 +1,14 @@
 package halot.nikitazolin.bot.discord.command.commands.music;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-
 import halot.nikitazolin.bot.discord.audio.GuildAudioService;
-import halot.nikitazolin.bot.discord.audio.player.IPlayerService;
 import halot.nikitazolin.bot.discord.audio.player.QueueFiller;
 import halot.nikitazolin.bot.discord.command.model.BotCommand;
 import halot.nikitazolin.bot.discord.command.model.BotCommandContext;
-import halot.nikitazolin.bot.repository.model.SongHistory;
 import halot.nikitazolin.bot.util.MessageUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,9 +23,9 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 @RequiredArgsConstructor
 public class PlayCommand extends BotCommand {
 
-  private final IPlayerService playerService;
   private final GuildAudioService guildAudioService;
   private final MessageUtil messageUtil;
+  private final QueueFiller queueFiller;
 
   @Override
   public String name() {
@@ -74,22 +69,18 @@ public class PlayCommand extends BotCommand {
 
   @Override
   public void execute(BotCommandContext context) {
-    AudioPlayer audioPlayer = playerService.getAudioPlayer();
-
-//    String url0 = "https://www.youtube.com/watch?v=pfgrI39PM0o";
-    String url0 = "D:\\Music\\Folders\\2024\\Kidd Russell - Fade (Минус).mp3";
+    String url0 = "https://www.youtube.com/watch?v=Tctv-vujFKU";
+//    String url0 = "D:\\Music\\Folders\\2024\\Kidd Russell - Fade (Минус).mp3";
     String url1 = "D:\\Music\\Folders\\2024\\Kidd Russell - Fade (Минус).mp3";
     String url2 = "D:\\Music\\Folders\\2023\\30 Seconds To Mars - Attack.mp3";
-    String url3 = "D:\\Music\\Folders\\2023\\30 Seconds To Mars - Attack.mp3";
-//    String url3 = "https://youtu.be/kS-Mob5Ha64?si=qlEmw8tKoEmmQhHs";
+//    String url3 = "D:\\Music\\Folders\\2023\\30 Seconds To Mars - Attack.mp3";
+    String url3 = "https://youtu.be/Tctv-vujFKU?si=731Seio46tir0SgO";
 
 //    List<String> links = context.getArgumentMapper().getString();
     List<String> links = List.of(url0, url1, url2, url3);
+    System.out.println("links size: " + links.size());
 
-    for (String trackUrl : links) {
-      playerService.getAudioPlayerManager().loadItemSync(trackUrl, new QueueFiller(playerService));
-//      playerService.getAudioPlayerManager().loadItem(trackUrl, new QueueFiller(playerService));
-    }
+    queueFiller.makeTracks(links);
 
     if (guildAudioService.connectToVoiceChannel(context) == false) {
       return;
@@ -97,9 +88,8 @@ public class PlayCommand extends BotCommand {
 
     guildAudioService.getPlayerService().startPlayingMusic();
 
-    EmbedBuilder embed = messageUtil.createSuccessEmbed("Play: " + audioPlayer.getPlayingTrack().getIdentifier());
+    EmbedBuilder embed = messageUtil.createSuccessEmbed("User launched audiotrack");
     context.sendMessageEmbed(embed);
-
-    log.debug("User launched audiotrack." + " User: " + context.getUser() + " Track: " + audioPlayer.getPlayingTrack().getIdentifier());
+    log.debug("User launched audiotrack." + " User: " + context.getUser());
   }
 }
