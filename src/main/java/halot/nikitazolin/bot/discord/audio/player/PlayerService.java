772875706596e3ae1f63f870a687a19f15542cc5
HALO-PC <nekitzollin@gmail.com>
2024-05-12 21:39:49 +0300

    0.5.0 - Update Lavaplayer

--- src/main/java/halot/nikitazolin/bot/discord/audio/player/PlayerService.java
+++ src/main/java/halot/nikitazolin/bot/discord/audio/player/PlayerService.java
@@ -10,14 +10,27 @@ import java.util.concurrent.LinkedBlockingQueue;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerRegistry;
 import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration.ResamplingQuality;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.beam.BeamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.getyarn.GetyarnAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.nico.NicoAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
+import dev.lavalink.youtube.clients.AndroidWithThumbnail;
+import dev.lavalink.youtube.clients.MusicWithThumbnail;
+import dev.lavalink.youtube.clients.WebWithThumbnail;
+import dev.lavalink.youtube.clients.skeleton.Client;
 import halot.nikitazolin.bot.discord.DatabaseService;
 import halot.nikitazolin.bot.discord.action.BotCommandContext;
 import lombok.Getter;
@@ -56,9 +69,20 @@ public class PlayerService implements AudioSendHandler {
   }
 
   public void createPlayer() {
-    AudioSourceManagers.registerRemoteSources(audioPlayerManager);
     AudioSourceManagers.registerLocalSource(audioPlayerManager);
-    audioPlayerManager.source(YoutubeAudioSourceManager.class).setPlaylistPageCount(10);
+
+    YoutubeAudioSourceManager youtube = new YoutubeAudioSourceManager(true,
+        new Client[] { new MusicWithThumbnail(), new WebWithThumbnail(), new AndroidWithThumbnail() });
+    audioPlayerManager.registerSourceManager(youtube);
+
+    audioPlayerManager.registerSourceManager(SoundCloudAudioSourceManager.createDefault());
+    audioPlayerManager.registerSourceManager(new BandcampAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new VimeoAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new TwitchStreamAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new BeamAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new GetyarnAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new NicoAudioSourceManager());
+    audioPlayerManager.registerSourceManager(new HttpAudioSourceManager(MediaContainerRegistry.DEFAULT_REGISTRY));
 
     audioPlayerManager.getConfiguration().setResamplingQuality(ResamplingQuality.HIGH);
     audioPlayerManager.getConfiguration().setOpusEncodingQuality(10);
